37642e6d692462993b4dc43ff3b4342dfb5a7f4b
Sean Ma <maye-msft@outlook.com>
2019-08-14 18:33:01 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'engagements-snapshot'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
